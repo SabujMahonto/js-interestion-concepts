ac90408c80f11fc2995e75da8caa4a65b0c8d13a
SabujMahonto <64306095+SabujMahonto@users.noreply.github.com>
2022-02-25 15:07:26 +0600

    double equal or tripal equal concept done

---
+++ double-tripal.js
@@ -0,0 +1,18 @@
+const first = 22;
+const second = '22';
+
+// if( first === second){
+//     console.log('condition is true');
+// }else{
+//     console.log('condition is false')
+// }
+
+
+const a ={ name: 'jon',}
+const b ={ name: 'jon',}
+
+if(a == b){
+    console.log('they are same')
+}else{
+    console.log('they are nat same')
+}
