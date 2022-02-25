b0e90c45e1ae025db1bcad0121164d2735e40b12
SabujMahonto <64306095+SabujMahonto@users.noreply.github.com>
2022-02-25 13:11:01 +0600

    truty or fulsy value concept clear

---
+++ true-false.js
@@ -0,0 +1,18 @@
+
+const x = false;
+if(x){
+    console.log('variable is truthy');
+}else{
+    console.log('variable is falsy');
+}
+
+// **falsy
+//         1.false ,
+//         2. 0 , 
+//         3.empty string , 
+//         4.undefined, 
+//         5.null, 
+//         6.NaN;
+
+// ** truthy 
+// or everything is truthy 
