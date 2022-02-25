2cd43658945ef8831ac31b0f656655e0af398635
SabujMahonto <64306095+SabujMahonto@users.noreply.github.com>
2022-02-25 12:32:06 +0600

    primitive vs non-primitime data type concepts done

---
+++ data-types.js
@@ -0,0 +1,6 @@
+let age = 20; 
+console.log(typeof age) // number
+let name = 'jarry cat';
+console.log(typeof name) //string
+let isStudent = true;
+console.log(typeof isStudent) //boolean
---
+++ primitive.js
@@ -0,0 +1,14 @@
+
+// primitive type 
+let a = 'Hello '
+let b = a;
+   a = "Hello World"
+console.log(a,b);
+
+// Non-Primitive 
+
+const x = {a:'Wev developer'}
+const y = x;
+x.job ="front end developer"
+console.log(x,y)
+
